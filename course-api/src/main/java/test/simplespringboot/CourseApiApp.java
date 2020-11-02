6017374b87fb2f26e86b557ca16b6562f5bdf6f7
I538130 <apoorv.semwal@sap.com>
2020-11-02 15:48:05 -0500

    Basic App Strcuture with some simple controllers

---
+++ course-api/src/main/java/test/simplespringboot/CourseApiApp.java
@@ -0,0 +1,13 @@
+package test.simplespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApiApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApiApp.class, args);
+	}
+
+}
