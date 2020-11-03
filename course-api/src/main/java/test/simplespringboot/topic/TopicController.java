a48bd4d888914adc184c411fc8b9993def29238e
I538130 <apoorv.semwal@sap.com>
2020-11-02 22:33:15 -0500

    Added GET API for a specific course ID

--- course-api/src/main/java/test/simplespringboot/topic/TopicController.java
+++ course-api/src/main/java/test/simplespringboot/topic/TopicController.java
@@ -3,6 +3,7 @@ package test.simplespringboot.topic;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,5 +16,10 @@ public class TopicController {
 	@RequestMapping("/topics")
 	public List<Topic> getAllTopics() {
 		return topicService.getAllTopics();
+	}
+	
+	@RequestMapping("/topics/{topic_id}")
+	public Topic getTopic(@PathVariable("topic_id") String id) {
+		return topicService.getTopic(id);
 	}
 }
