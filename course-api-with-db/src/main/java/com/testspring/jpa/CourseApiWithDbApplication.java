245202aeb197af69fa7d26557731a0204e6333b5
I538130 <apoorv.semwal@sap.com>
2020-11-03 11:40:10 -0500

    Basic course API with JPA - Uses Apache Derby as an embedded DB

---
+++ course-api-with-db/src/main/java/com/testspring/jpa/CourseApiWithDbApplication.java
@@ -0,0 +1,13 @@
+package com.testspring.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApiWithDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApiWithDbApplication.class, args);
+	}
+
+}
---
+++ course-api-with-db/src/test/java/com/testspring/jpa/CourseApiWithDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testspring.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApiWithDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
