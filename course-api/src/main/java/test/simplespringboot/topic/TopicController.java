e7c9ca88720f04171a4a89aaec0a21b98ba2e622
I538130 <apoorv.semwal@sap.com>
2020-11-03 00:23:27 -0500

    Added new APIs for Update and Delete Topic

--- course-api/src/main/java/test/simplespringboot/topic/TopicController.java
+++ course-api/src/main/java/test/simplespringboot/topic/TopicController.java
@@ -11,22 +11,32 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TopicController {
-	
+
 	@Autowired
 	private TopicService topicService;
-	
+
 	@RequestMapping("/topics")
 	public List<Topic> getAllTopics() {
 		return topicService.getAllTopics();
 	}
-	
+
 	@RequestMapping("/topics/{topic_id}")
 	public Topic getTopic(@PathVariable("topic_id") String id) {
 		return topicService.getTopic(id);
 	}
-	
+
 	@RequestMapping(method=RequestMethod.POST, value="/topics")
 	public void addTopic(@RequestBody Topic topic) {
 		topicService.addTopic(topic);
 	}
+
+	@RequestMapping(method=RequestMethod.PUT, value="/topics/{id}")
+	public void updateTopic(@RequestBody Topic topic, @PathVariable String id) {
+		topicService.updateTopic(topic, id);
+	}	
+
+	@RequestMapping(method=RequestMethod.DELETE, value="/topics/{id}")
+	public void deleteTopic(@PathVariable String id) {
+		topicService.deleteTopic(id);
+	}	
 }
--- course-api/src/main/java/test/simplespringboot/topic/TopicService.java
+++ course-api/src/main/java/test/simplespringboot/topic/TopicService.java
@@ -3,6 +3,7 @@ package test.simplespringboot.topic;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,10 +18,23 @@ public class TopicService {
 	}
 
 	public Topic getTopic(String id){
-		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+		return topics.stream().filter(t -> t.getId().equalsIgnoreCase(id)).findFirst().get();
 	}
 
 	public void addTopic(Topic topic) {
 		topics.add(topic);
 	}
+
+	public void updateTopic(Topic topic, String id) {
+		for (int i = 0; i < topics.size(); i++) {
+			if(topics.get(i).getId().equalsIgnoreCase(id)) {
+				topics.set(i, topic);
+				return;
+			}
+		}
+	}
+
+	public void deleteTopic(String id) {
+		topics.removeIf(t -> t.getId().equalsIgnoreCase(id));
+	}	
 }
