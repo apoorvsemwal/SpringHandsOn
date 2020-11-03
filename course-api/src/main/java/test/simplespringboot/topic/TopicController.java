22b9f0d8809a048f39ce3525da0739e630c95927
I538130 <apoorv.semwal@sap.com>
2020-11-02 23:47:44 -0500

    New API Added - POST - Add a new Topic

--- course-api/src/main/java/test/simplespringboot/topic/TopicController.java
+++ course-api/src/main/java/test/simplespringboot/topic/TopicController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,5 +23,10 @@ public class TopicController {
 	@RequestMapping("/topics/{topic_id}")
 	public Topic getTopic(@PathVariable("topic_id") String id) {
 		return topicService.getTopic(id);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST, value="/topics")
+	public void addTopic(@RequestBody Topic topic) {
+		topicService.addTopic(topic);
 	}
 }
--- course-api/src/main/java/test/simplespringboot/topic/TopicService.java
+++ course-api/src/main/java/test/simplespringboot/topic/TopicService.java
@@ -1,15 +1,16 @@
 package test.simplespringboot.topic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TopicService {
-	private List<Topic> topics = Arrays.asList(
+	private List<Topic> topics = new ArrayList<>(Arrays.asList(
 			new Topic("Spring", "Spring Framework", "Dependency Injection"),
 			new Topic("Java", "Language", "Frameworks")
-			);
+			));
 
 	public List<Topic> getAllTopics(){
 		return topics;
@@ -17,5 +18,9 @@ public class TopicService {
 
 	public Topic getTopic(String id){
 		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+
+	public void addTopic(Topic topic) {
+		topics.add(topic);
 	}
 }
