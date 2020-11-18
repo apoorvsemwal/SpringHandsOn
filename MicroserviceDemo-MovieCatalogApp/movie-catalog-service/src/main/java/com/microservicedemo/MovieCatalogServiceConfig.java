af38d9f294bbff6ba2ac0898b634f43a1249395b
Apoorv Semwal <apoorv.semwal20@gmail.com>
2020-11-18 14:05:44 -0500

    Commented out WebClient and bought back the RestTemplate

--- MicroserviceDemo-MovieCatalogApp/movie-catalog-service/src/main/java/com/microservicedemo/MovieCatalogServiceConfig.java
+++ MicroserviceDemo-MovieCatalogApp/movie-catalog-service/src/main/java/com/microservicedemo/MovieCatalogServiceConfig.java
@@ -3,7 +3,7 @@ package com.microservicedemo;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
+//import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class MovieCatalogServiceConfig {
@@ -13,8 +13,8 @@ public class MovieCatalogServiceConfig {
 		return new RestTemplate();
 	}
 
-	@Bean
-	public WebClient.Builder getWebClientBuilder() {
-		return WebClient.builder();
-	}	
+//	@Bean
+//	public WebClient.Builder getWebClientBuilder() {
+//		return WebClient.builder();
+//	}	
 }
--- MicroserviceDemo-MovieCatalogApp/movie-catalog-service/src/main/java/com/microservicedemo/resources/MovieCatalogResource.java
+++ MicroserviceDemo-MovieCatalogApp/movie-catalog-service/src/main/java/com/microservicedemo/resources/MovieCatalogResource.java
@@ -8,8 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-//import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.client.RestTemplate;
+//import org.springframework.web.reactive.function.client.WebClient;
 
 import com.microservicedemo.models.CatalogItem;
 import com.microservicedemo.models.Movie;
@@ -19,11 +19,11 @@ import com.microservicedemo.models.Rating;
 @RequestMapping("/catalog")   //Root URL - Redirect any URL starting with /catalog to this class
 public class MovieCatalogResource {
 	
-//	@Autowired
-//	private RestTemplate restTemplate;
-
 	@Autowired
-	private WebClient.Builder webClientBuilder;
+	private RestTemplate restTemplate;
+
+//	@Autowired
+//	private WebClient.Builder webClientBuilder;
 	
 	@RequestMapping("/{userId}") // equivalent to - /catalog/userId - /catalog comes from class level.
 	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
@@ -38,15 +38,15 @@ public class MovieCatalogResource {
 		//is coming from movie-info-service.
 		return ratings.stream().map(rating -> {
 			
-			//Movie movie = restTemplate.getForObject("http://localhost:8082/movies/"+rating.getMovieId(), Movie.class);
+			Movie movie = restTemplate.getForObject("http://localhost:8082/movies/"+rating.getMovieId(), Movie.class);
 			
 			//Using WebClient in place of RestTemplate - Rest of the logic remains the same.
-			Movie movie = webClientBuilder.build()
-					.get()
-					.uri("http://localhost:8082/movies/"+rating.getMovieId())
-					.retrieve()
-					.bodyToMono(Movie.class)
-					.block();
+//			Movie movie = webClientBuilder.build()
+//					.get()
+//					.uri("http://localhost:8082/movies/"+rating.getMovieId())
+//					.retrieve()
+//					.bodyToMono(Movie.class)
+//					.block();
 						
 			return new CatalogItem(movie.getName(), "Test", rating.getRating());
 		}).collect(Collectors.toList());
