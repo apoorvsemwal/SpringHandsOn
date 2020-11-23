bfcb03624a07b052685475ba298de94e26dc79de
Apoorv Semwal <apoorv.semwal20@gmail.com>
2020-11-23 10:24:21 -0500

    Added Timeout option within the RestTemplate

--- MicroserviceDemo-MovieCatalogApp/movie-catalog-service/src/main/java/com/microservicedemo/MovieCatalogServiceConfig.java
+++ MicroserviceDemo-MovieCatalogApp/movie-catalog-service/src/main/java/com/microservicedemo/MovieCatalogServiceConfig.java
@@ -3,6 +3,7 @@ package com.microservicedemo;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 //import org.springframework.web.reactive.function.client.WebClient;
 
@@ -12,7 +13,10 @@ public class MovieCatalogServiceConfig {
 	@Bean
 	@LoadBalanced
 	public RestTemplate getRestTemplate() {
-		return new RestTemplate();
+		//Allows us to set a time-out to avoid slowing down the server and reclaim threads back on time.
+		HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory(); 
+		clientHttpRequestFactory.setConnectTimeout(3000);
+		return new RestTemplate(clientHttpRequestFactory);
 	}
 
 	//	@Bean
