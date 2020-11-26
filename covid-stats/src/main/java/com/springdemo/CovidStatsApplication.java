582b7451d4fa3aab2c257df48044c6499982c1b7
Apoorv Semwal <apoorv.semwal20@gmail.com>
2020-11-26 09:18:57 -0500

    Basic Spring Boot App Structure with required dependencies.

---
+++ covid-stats/src/main/java/com/springdemo/CovidStatsApplication.java
@@ -0,0 +1,13 @@
+package com.springdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidStatsApplication.class, args);
+	}
+
+}
