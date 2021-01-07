da03c38e3dfdb50a0250262ce588eb2c981a5156
Apoorv Semwal <apoorv.semwal20@gmail.com>
2021-01-07 16:54:36 -0500

    Added a new API Gateway for routing requests to Microservices

---
+++ MicroserviceDemo-UserDepartment/user-department-api-gateway/src/main/java/com/springdemo/UserDepartmentApiGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.springdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class UserDepartmentApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserDepartmentApiGatewayApplication.class, args);
+	}
+
+}
